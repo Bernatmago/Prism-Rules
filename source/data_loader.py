8794e611a2750d20c71fc84d5a233aa5f1a3e968
Bernat Martinez <bolicubo@gmail.com>
2020-03-11 14:08:19 +0100

    First working version (heuristics missing)

--- source/data_loader.py
+++ source/data_loader.py
@@ -1,24 +1,31 @@
 import pandas as pd
+import numpy as np
 from os.path import join
 from scipy.io import arff
 
+
 # Large 2000+
 def load_data(filename):
-    data_path = './data'
+    data_path = '../data'
     if filename.endswith('.arff'):
-        data = arff.loadarff(join(data_path,filename))
+        data = arff.loadarff(join(data_path, filename))
         df = pd.DataFrame(data[0])
-    else:    
-        df = pd.read_csv(join(data_path,filename))
+    else:
+        df = pd.read_csv(join(data_path, filename), sep=',|;', engine='python')
 
-    y = df.to_numpy()[:,-1]
-    X = df.to_numpy()[:,:-1]
-    return X, y
+    y = df.to_numpy()[:, -1].astype(str)
+    X = df.to_numpy()[:, :].astype(str)
+    x_names = {}
+    for i, v in enumerate(list(df.columns)[:-1]):
+        x_names[v + '__' + str(i)] = np.unique(X[:, i])
+    return X, y, x_names, np.unique(X[:, -1])
+
+
 # Medium 500-2000
 
 # Small 0-500
 if __name__ == "__main__":
-    X, y = load_data('mushroom.csv')
-    X, y = load_data('divorce.csv')
-    X, y = load_data('student_performance.arff')
-    pass
+    X, y, x_names, y_names = load_data('mushroom.csv')
+    X, y, x_names, y_names = load_data('divorce.csv')
+    X, y, x_names, y_names = load_data('student_performance.arff')
+    print(1)
